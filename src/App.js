bdca2b05459f375038a01118cb58294b26fb5e2e
Jisan <jishananam@iut-dhaka.edu>
2022-05-21 10:10:38 +0600

    almost complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import RandomArray from './Components/array'
+import SortVisualizer from './Components/sortVisualizer/SortVisualizer'
 function App() {
   return (
     <div className="App">
-      <RandomArray/>
+      <SortVisualizer/>
     </div>
   );
 }
